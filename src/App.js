d77e4c2997ae831da7385b1e2efa6b80c6021bc5
Lucas Vilar <lucas.vilar360@gmail.com>
2023-07-12 23:11:21 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,192 @@
 import logo from './logo.svg';
 import './App.css';
+import BarChart from './components/BarChart';
+import { useEffect, useState } from 'react';
+import { Data, consulta1example, consulta3example, consulta4example } from './Data';
+import { Chart as ChartJS } from 'chart.js/auto'
+import LineChart from './components/LineChart';
+import PieChart from './components/PieChart';
+import axios from 'axios';
 
 function App() {
+  const [consulta1, setConsulta1] = useState(null);
+  const [consulta2, setConsulta2] = useState(null);
+  const [consulta3, setConsulta3] = useState(null);
+  const [consulta4, setConsulta4] = useState(null);
+  const [consulta5, setConsulta5] = useState(null);
+
+
+
+  const options_consulta_1 = {
+    indexAxis: 'y', // Define o eixo x como o eixo horizontal
+
+    scales: {
+      x: {
+        beginAtZero: true,
+        ticks: {
+          stepSize: 1,
+
+        },
+      },
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+
+  useEffect(() => {
+    const fetchData1 = async () => {
+      try {
+        const response = await axios.get('http://localhost:3030/consulta1');
+        setConsulta1({
+          labels: response.data.map((data) => data.senador),
+          datasets: [{
+            label: "Quantidade de partidos já filiados",
+            data: response.data.map((data) => data.partidos_senador),
+            backgroundColor: [
+              "#2250bd",
+            ],
+            // borderColor: "black",
+            // borderWidth: 2,
+          }]
+        });
+      } catch (error) {
+        console.error('Erro ao fazer a requisição:', error);
+      }
+    };
+
+    const fetchData2 = async () => {
+      try {
+        const response = await axios.get('http://localhost:3030/consulta2');
+        setConsulta2({
+          labels: response.data.map((data) => data.bancada),
+          datasets: [{
+            label: "Quantidade de relatorias",
+            data: response.data.map((data) => data.relatorias),
+            backgroundColor: [
+              "#eb4034",
+              "#2250bd"
+
+            ],
+            // borderColor: "black",
+            // borderWidth: 2,
+          }]
+        });
+      } catch (error) {
+        console.error('Erro ao fazer a requisição:', error);
+      }
+    };
+
+    const fetchData3 = async () => {
+      try {
+        const response = await axios.get('http://localhost:3030/consulta3');
+        setConsulta3({
+          labels: response.data.map((data) => data.partido),
+          datasets: [{
+            label: "Quantidade de autores",
+            data: response.data.map((data) => data.materias),
+            backgroundColor: [
+              "#eb4034",
+              "#2250bd",
+              "#3c9929"
+            ],
+            // borderColor: "black",
+            // borderWidth: 2,
+          }]
+        });
+      } catch (error) {
+        console.error('Erro ao fazer a requisição:', error);
+      }
+    };
+
+    const fetchData4 = async () => {
+      try {
+        const response = await axios.get('http://localhost:3030/consulta4');
+        setConsulta4({
+          labels: response.data.map((data) => data.bancada),
+          datasets: [{
+            label: "Quantidade de partidos já filiados",
+            data: response.data.map((data) => data.discursos),
+            backgroundColor: [
+              "#2250bd",
+            ],
+            // borderColor: "black",
+            // borderWidth: 2,
+          }]
+        });
+      } catch (error) {
+        console.error('Erro ao fazer a requisição:', error);
+      }
+    };
+
+    const fetchData5 = async () => {
+      try {
+        const response = await axios.get('http://localhost:3030/consulta5');
+        console.log(response.data)
+        setConsulta5({
+          labels: response.data.map((data) => data.senador),
+          datasets: [{
+            label: "Quantidade de relatorias",
+            data: response.data.map((data) => data.relatorias),
+            backgroundColor: [
+              "#2250bd",
+            ],
+            // borderColor: "black",
+            // borderWidth: 2,
+          }]
+        });
+      } catch (error) {
+        console.error('Erro ao fazer a requisição:', error);
+      }
+    };
+
+    fetchData1();
+    fetchData2();
+    fetchData3();
+    fetchData4();
+    fetchData5();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h2>Top 3 senadores que mais trocaram de partido</h2>
+      {consulta1 !== null && (
+        <div className="Grafico1">
+          <BarChart chartData={consulta1} options={options_consulta_1} />
+        </div>
+      )}
+
+      <h2>Quantidade de relatorias dos top 2 blocos parlamentares com mais membros</h2>
+      {consulta2 !== null && (
+        <div className="Grafico2">
+          <PieChart chartData={consulta2} />
+        </div>
+      )}
+
+      <h2>Top 3 partidos com mais autores</h2>
+      {consulta3 !== null && (
+        <div className="Grafico2">
+          <PieChart chartData={consulta3} />
+        </div>
+      )}
+
+      <h2>Ranqueamento de bancadas por total de discursos por senador</h2>
+      {consulta4 !== null && (
+        <div className="Grafico4">
+          <BarChart chartData={consulta4}  />
+        </div>
+      )}
+      
+      
+      <h2>Relatorias por senador</h2>
+      {consulta5 !== null && (
+        <div className="Grafico5">
+          <BarChart chartData={consulta5}  options={options_consulta_1}/>
+        </div>
+      )}
+
+    
     </div>
   );
 }
---
+++ src/Data.js
@@ -0,0 +1,126 @@
+export const Data = [
+    {
+        id: 1,
+        year: 2016,
+        userGain: 800,
+        userLost: 231,
+    },
+    {
+        id: 2,
+        year: 2017,
+        userGain: 700,
+        userLost: 231,
+    },
+    {
+        id: 3,
+        year: 2018,
+        userGain: 1000,
+        userLost: 231,
+    },
+    {
+        id: 4,
+        year: 2018,
+        userGain: 500,
+        userLost: 231,
+    }
+
+]
+
+export const consulta1example = [
+    {
+        codigo: 3806,
+        nome: "Zequinha Marinho",
+        quantidade: 4
+    },
+    {
+        codigo: 5812,
+        nome: "Bernardo Ventura",
+        quantidade: 4
+    },
+    {
+        codigo: 5982,
+        nome: "Alessandro Vieira",
+        quantidade: 3
+    }, 
+    {
+        codigo: 5672,
+        nome: "Alan Rick",
+        quantidade: 2
+    },
+    {
+        codigo: 1234,
+        nome: "Benjamin Connan",
+        quantidade: 2
+    },
+    {
+        codigo: 3214,
+        nome: "Benjamin Connan",
+        quantidade: 2
+    },
+    {
+        codigo: 6123,
+        nome: "Benjamin Connan",
+        quantidade: 2
+    },
+    {
+        codigo: 5617,
+        nome: "Benjamin Connan",
+        quantidade: 2
+    },
+    {
+        codigo: 8964,
+        nome: "Benjamin Connan",
+        quantidade: 2
+    },
+    {
+        codigo: 5827,
+        nome: "Benjamin Connan",
+        quantidade: 2
+    },
+]
+
+export const consulta3example = [
+    {
+        codigo: 1,
+        nome: "Nome do primeiro partido",
+        quantidade: 6
+    },
+    {
+        codigo: 2,
+        nome: "Nome do segundo partido",
+        quantidade: 4
+    },
+    {
+        codigo: 3,
+        nome: "Nome do terceiro partido",
+        quantidade: 3
+    },
+]
+
+export const consulta4example = [
+    {
+        codigo: 3806,
+        nome: "Zequinha Marinho",
+        quantidade: 4
+    },
+    {
+        codigo: 5812,
+        nome: "Bernardo Ventura",
+        quantidade: 4
+    },
+    {
+        codigo: 5982,
+        nome: "Alessandro Vieira",
+        quantidade: 3
+    }, 
+    {
+        codigo: 5672,
+        nome: "Alan Rick",
+        quantidade: 2
+    },
+    {
+        codigo: 1234,
+        nome: "Benjamin Connan",
+        quantidade: 2
+    },
+]
